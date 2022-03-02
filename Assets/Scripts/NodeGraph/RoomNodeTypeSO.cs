7d7a783d031b17e6f94bffab3b8377f8d311d9ba
Dinis Costa <61733414+scorpaust@users.noreply.github.com>
2022-03-02 20:20:49 +0000

    First commit - Dungeon Room Graph Editor setup.

---
+++ Assets/Scripts/NodeGraph/RoomNodeTypeSO.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RoomNodeType_", menuName = "Scriptable Objects/Dungeon/Room Node Type")]
+
+public class RoomNodeTypeSO : ScriptableObject
+{
+    public string roomNoodeTypeName;
+
+	#region Header
+	[Header("Only flag the RoomNodeTypes that should be visible in the Editor.")]
+	#endregion Header
+	public bool displayInNodeGraphEditor = true;
+
+	#region Header
+	[Header("One type should be a Corridor.")]
+	#endregion Header
+	public bool isCorridor;
+
+	#region Header
+	[Header("One type should be a CorridorNS.")]
+	#endregion Header
+	public bool isCorridorNS;
+
+	#region Header
+	[Header("One type should be a CorridorEW.")]
+	#endregion Header
+	public bool isCorridorEW;
+
+	#region Header
+	[Header("One type should be an Entrance.")]
+	#endregion Header
+	public bool isEntrance;
+
+	#region Header
+	[Header("One type should be a Boss Room.")]
+	#endregion Header
+	public bool isBossRoom;
+
+	#region Header
+	[Header("One type should be None (Unassigned).")]
+	#endregion Header
+	public bool isNone;
+
+}
