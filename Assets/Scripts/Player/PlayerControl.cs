8d9acf2f85086c800a9b7e6f78b85b5f762c1d16
Dinis Costa <scorpaust@hotmail.com>
2022-04-14 12:06:12 +0100

    Switching Player Weapons

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -152,6 +152,9 @@ public class PlayerControl : MonoBehaviour
 		// Fire weapon input
 		FireWeaponInput(weaponDirection, weaponAngleDegrees, playerAngleDegrees, playerAimDirection);
 
+		// Switch weapon input
+		SwitchWeaponInput();
+
 		// Reload weapon input
 		ReloadWeaponInput();
 	}
@@ -189,6 +192,75 @@ public class PlayerControl : MonoBehaviour
 		}
 	}
 
+	private void SwitchWeaponInput()
+	{
+		// Switch weapon if mouse scroll wheel selected
+		if (Input.mouseScrollDelta.y < 0f)
+		{
+			PreviousWeapon();
+		}
+
+		if (Input.mouseScrollDelta.y > 0f)
+		{
+			NextWeapon();
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			SetWeaponByIndex(1);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			SetWeaponByIndex(2);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			SetWeaponByIndex(3);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha4))
+		{
+			SetWeaponByIndex(4);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha5))
+		{
+			SetWeaponByIndex(5);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha6))
+		{
+			SetWeaponByIndex(6);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha7))
+		{
+			SetWeaponByIndex(7);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha8))
+		{
+			SetWeaponByIndex(8);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha9))
+		{
+			SetWeaponByIndex(9);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha0))
+		{
+			SetWeaponByIndex(10);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Minus))
+		{
+			SetCurrentWeaponToFirstInTheList();
+		}
+	}
+
 	private void SetWeaponByIndex(int weaponIndex)
 	{
 		if (weaponIndex - 1 < player.weaponList.Count)
@@ -197,6 +269,30 @@ public class PlayerControl : MonoBehaviour
 
 			player.setActiveWeaponEvent.CallSetActiveWeaponEvent(player.weaponList[weaponIndex - 1]);
 		}
+	}
+
+	private void NextWeapon()
+	{
+		currentWeaponIndex++;
+
+		if (currentWeaponIndex > player.weaponList.Count)
+		{
+			currentWeaponIndex = 1;
+		}
+
+		SetWeaponByIndex(currentWeaponIndex);
+	}
+
+	private void PreviousWeapon()
+	{
+		currentWeaponIndex--;
+
+		if (currentWeaponIndex < 1)
+		{
+			currentWeaponIndex = player.weaponList.Count;
+		}
+
+		SetWeaponByIndex(currentWeaponIndex);
 	}
 
 	private void ReloadWeaponInput()
@@ -237,6 +333,41 @@ public class PlayerControl : MonoBehaviour
 
 			isPlayerRolling = false;
 		}
+	}
+
+	private void SetCurrentWeaponToFirstInTheList()
+	{
+		// Create new temporary list
+		List<Weapon> tempWeaponList = new List<Weapon>();
+
+		// Add the current weapon to first in the temp list
+		Weapon currentWeapon = player.weaponList[currentWeaponIndex - 1];
+
+		currentWeapon.weaponListPosition = 1;
+
+		tempWeaponList.Add(currentWeapon);
+
+		// Loop through existing weapon list and add - skipping current weapon
+		int index = 2;
+
+		foreach (Weapon weapon in player.weaponList)
+		{
+			if (weapon == currentWeapon) continue;
+
+			tempWeaponList.Add(weapon);
+
+			weapon.weaponListPosition = index;
+
+			index++;
+		}
+
+		// Assign new list
+		player.weaponList = tempWeaponList;
+
+		currentWeaponIndex = 1;
+
+		// Set current weapon
+		SetWeaponByIndex(currentWeaponIndex);
 	}
 
 	#region Validation 
